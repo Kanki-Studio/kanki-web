66639de5f1a9bfdff4d2d9ee25b3e042094ce754
Roland Enola <aloneroland@gmail.com>
2023-07-20 21:26:53 +0100

    new design

---
+++ src/app/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="bg-black-100 text-white px-10 py-4 w-full">
+            <div className=" h-auto flex flex-col gap-5 xl:flex-row justify-between">
+                <h4 className="m-0 font-black text-2xl">Kanki Studios</h4>
+                Footer
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
